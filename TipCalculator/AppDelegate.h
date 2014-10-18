7a0baadf086ed930ca19538614c786fd2e2018d9
Darlene Wong <dwong@dwong-mac-air.local>
2014-10-18 10:35:11 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Darlene Wong on 10/18/14.
+//  Copyright (c) 2014 Darlene Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
